44bebefd3dbfdcc60fefe676e0b6bf40d14ede9e
Emre Yuz <emreyuz@sabanciuniv.edu>
2021-01-20 23:50:34 +0300

    20/01 ders sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+
+        
+    {
+
+        int i = 1;
+
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " numaralı müşteri eklendi: " + musteri.Ad + " " + musteri.soyad);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " numaralı müşteri silindi: " + musteri.Ad + " " + musteri.soyad);
+        }
+
+        public void List(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + ": " + musteri.Ad + " " + musteri.soyad);
+            }
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Mark";
+            musteri1.soyad = "Knopfler";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Eric";
+            musteri2.soyad = "Clapton";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Aretha";
+            musteri3.soyad = "Franklin";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+
+            musteriManager.List(musteriler);
+
+            musteriManager.Delete(musteri2);
+
+           
+
+
+
+
+        }
+    }
+}
